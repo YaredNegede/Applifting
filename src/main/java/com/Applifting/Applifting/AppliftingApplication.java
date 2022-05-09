317b2aa2c4c58fd3c2e8e11b5fc7cccc22c81e81
Yared Negede <yaredngd@gmail.com>
2022-05-09 11:16:09 +0300

    added trace repo

--- src/main/java/com/Applifting/Applifting/AppliftingApplication.java
+++ src/main/java/com/Applifting/Applifting/AppliftingApplication.java
@@ -1,15 +1,18 @@
 package com.Applifting.Applifting;
 
-import com.Applifting.Applifting.common.MonitorFilter;
+import com.Applifting.Applifting.monitor.AppliftHttpTraceRepository;
+import lombok.extern.java.Log;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTrace;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.List;
+
+@Log
 @SpringBootApplication
 public class AppliftingApplication implements WebMvcConfigurer {
 
@@ -22,11 +25,24 @@ public class AppliftingApplication implements WebMvcConfigurer {
 		return new ModelMapper();
 	}
 
-	@Autowired
-	MonitorFilter logInterceptor;
+	@Bean
+	public HttpTraceRepository httpTraceRepository(){
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(logInterceptor);
+		return new AppliftHttpTraceRepository() {
+
+			@Override
+			public List<HttpTrace> findAll() {
+				log.info("******findAll*********");
+				return null;
+			}
+
+			@Override
+			public void add(HttpTrace trace) {
+					log.info("******add*********");
+			}
+
+		};
+
 	}
+
 }
---
+++ src/main/java/com/Applifting/Applifting/monitor/AppliftHttpTraceRepository.java
@@ -0,0 +1,8 @@
+package com.Applifting.Applifting.monitor;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+
+public interface AppliftHttpTraceRepository extends HttpTraceRepository {
+
+
+}
